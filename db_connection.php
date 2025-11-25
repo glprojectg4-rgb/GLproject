35d0d29a30ca1b9fdd4337ddb2f6c2675a248b83
glprojectg4-rgb <glprojectg4@gmail.com>
2025-11-26 00:49:50 +0100

    qq

---
+++ db_connection.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$db = 'blood_management';      // اسم قاعدة البيانات التي أنشأتها في phpMyAdmin
+$user = 'GL_root';            // اسم المستخدم الافتراضي في MAMP
+$pass = 'GL.Root.360';            // كلمة المرور الافتراضية في MAMP
+
+// إذا كنت على macOS، أضف رقم المنفذ (عادة 8889)
+$port = 3306; // أو 3306 في Windows
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
+}
+?>
